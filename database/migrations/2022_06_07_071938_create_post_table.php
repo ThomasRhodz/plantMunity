e2ba8100f3f7e1a637d58820e677019d44514cea
John Eliezar Rodis <J.rodis.477524@umindanao.edu.ph>
2022-06-08 20:44:22 +0800

    initial commit

---
+++ database/migrations/2022_06_07_071938_create_post_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->string('post_caption')->nullable();
+            $table->integer('number_of_likes')->nullable();
+            $table->integer('number_of_shares')->nullable();
+            $table->integer('number_of_comments')->nullable();
+            $table->string('status');
+            $table->timestamps();
+            $table->foreign('user_id')->references('uid')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+};
